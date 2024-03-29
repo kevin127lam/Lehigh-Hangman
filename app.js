343384553b110324901e4ca8ea68c6f952d42edd
Kevin Lam <kevin127lam@Gmail.com>
2024-03-29 10:06:24 -0400

    added functionality to make the artist values listed by default

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-// Put your name and ID here 
-
+//Kevin Lam - khl225
+const db = require('better-sqlite3')('top40.db');
 const express = require("express");
 const path = require("path");
-
 const app = express();
 
 app.use(express.static(
   path.resolve(__dirname, "public")
 ));
+
+
+app.get('/artists', (req, res) => {
+  const query = db.prepare('SELECT DISTINCT artist FROM songlist');
+  const artists = query.all().map(row => row.artist);
+  res.json(artists);
+});
 
 app.listen(3000, () => console.log("Starting up Top 40 Search"));
 
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,29 @@
 
+//calls init function
+$(() => {
+    $.ajax(
+        "/artists",
+        {
+            type: "GET",
+            dataType: "json",
+            success: function (data) {
+                loadArtists(data);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert("Error: " + jqXHR.responseText);
+                alert("Error: " + textStatus);
+                alert("Error: " + errorThrown);
+            }
+        }
+    );
+});
+
+// Function to fetch artist names from server and populate dropdown
+function loadArtists(artists) {
+    $("#artists").empty();
+    $("#artists").append(`<option value=""></option>`);
+    for (let artist of artists) {
+        $("#artists").append(`<option value = ${artist}>${artist}</option>`);
+    }
+}
+;
