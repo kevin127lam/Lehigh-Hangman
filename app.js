bb333b4e051e285d8239a6008cd0419d1bbb2008
Kevin Lam <kevin127lam@Gmail.com>
2024-03-30 11:03:22 -0400

    added functionality to search by artist

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 //Kevin Lam - khl225
+
 const db = require('better-sqlite3')('top40.db');
 const express = require("express");
 const path = require("path");
+const { off } = require('process');
 const app = express();
 
 app.use(express.static(
@@ -15,6 +17,27 @@ app.get('/artists', (req, res) => {
   res.json(artists);
 });
 
+// Endpoint to fetch songs based on search criteria
+app.get('/songs', (req, res) => {
+  const { artist, keyword, displayPerPage, pageNum } = req.query;
+  const offset = (pageNum - 1) * displayPerPage;
+  let query = 'SELECT * FROM songlist';
+  if (artist || keyword) {
+      query += ' WHERE ';
+      if (artist) {
+          query += `artist = '${artist}'`;
+          //if (keyword) query += ' AND ';
+      }
+      if (keyword) {
+          query += `title LIKE '%${keyword}%'`;
+      }
+  }
+
+  query += ` LIMIT ${displayPerPage} OFFSET ${offset}`;
+  const songs = db.prepare(query).all();
+  res.json({ songs });
+});
+
+
+
 app.listen(3000, () => console.log("Starting up Top 40 Search"));
-
-
--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,3 @@
-
-//calls init function
 $(() => {
     $.ajax(
         "/artists",
@@ -16,14 +14,93 @@ $(() => {
             }
         }
     );
+    $("#search").click(function () {
+        //arguments
+        let artist = $("#artists").val();
+        let keyword = $("#keyword").val();
+        let displayPerPage = $("#display").val();
+        let pageNum = 1;
+
+        // Log the query parameters to the console for error checking
+        console.log("Artist:", artist);
+        console.log("Keyword:", keyword);
+        console.log("Display Per Page:", displayPerPage);
+
+        //alert(keyword);
+        fetchSongs(artist, keyword, displayPerPage, pageNum);
+    });
+
+    //functions to handle previous and next button click
+    $("#prev").click(function () {
+        let artist = $("#artists").val();
+        let keyword = $("#keyword").val();
+        let displayPerPage = $("#display").val();
+        let pageNum = parseInt($("#page-num").text()) - 1;
+        fetchSongs(artist, keyword, displayPerPage, pageNum);
+    });
+
+    $("#next").click(function () {
+        let artist = $("#artists").val();
+        let keyword = $("#keyword").val();
+        let displayPerPage = $("#display").val();
+        let pageNum = parseInt($("#page-num").text()) + 1;
+        fetchSongs(artist, keyword, displayPerPage, pageNum);
+    });
 });
+
+// get the songs based on params
+function fetchSongs(artist, keyword, displayPerPage, pageNum) {
+    $.ajax({
+        url: "/songs",
+        type: "GET",
+        dataType: "json",
+        data: {
+            artist: artist,
+            keyword: keyword,
+            displayPerPage: displayPerPage,
+            pageNum: pageNum
+        },
+        success: function (data) {
+            displaySongs(data);
+            $("#page-num").text(pageNum);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.error("Error: " + errorThrown);
+        }
+    });
+}
+
+//print songs and values onto the table
+function displaySongs(data) {
+    $("#songs").empty();
+    let songs = data.songs;
+    for (let song of songs) {
+        if (song.numone > 0) {
+            $("#songs").append(`<tr class = "number-one"><td>${song.id}</td><td>${song.title}</td><td>${song.artist}</td></tr>`);
+        }
+        else {
+            $("#songs").append(`<tr><td>${song.id}<td>${song.title}</td><td>${song.artist}</td></tr>`);
+        }
+    }
+
+    // Show/hide prev/next buttons based on current page
+    const currentPage = parseInt($("#page-num").text());
+
+    //attempt to "blur out" next button once the end is reach *NOT WORKING*
+    const totalPageCount = Math.ceil(data.totalCount / $("#display").val());
+    if (currentPage === totalPageCount) {
+        $("#next").prop("disabled", true);
+    } else {
+        $("#next").prop("disabled", false);
+    }
+}
 
 // Function to fetch artist names from server and populate dropdown
 function loadArtists(artists) {
     $("#artists").empty();
     $("#artists").append(`<option value=""></option>`);
     for (let artist of artists) {
-        $("#artists").append(`<option value = ${artist}>${artist}</option>`);
+        $("#artists").append(`<option value = "${artist}">${artist}</option>`);
     }
 }
-;
+
