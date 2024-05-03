e3b82b382f39f9e1c0d78a84e12360c2d8130b0f
Kevin Lam <kevin127lam@gmail.com>
2024-05-03 11:22:42 -0400

    edited formatting and visuals

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,11 @@
-const keyboardDiv = document.querySelector(".keyboard");
 // Function to fetch a random word from the server
 function fetchRandomWord() {
     fetch('/random-word')
         .then(response => response.json())
         .then(data => {
             const { word, hint } = data;
+            document.querySelector(".hint-display p").innerText = hint;
+            document.querySelector(".word-display").innerHTML = word.split("").map(() => `<li class="letter"></li>`).join("");
             console.log(`Word: ${word}, Hint: ${hint}`);
         })
         .catch(error => {
@@ -15,7 +16,7 @@ function fetchRandomWord() {
 for (let index = 97; index <= 122; index++) {
     const button = document.createElement("button");
     button.innerText = String.fromCharCode(index);
-    keyboardDiv.appendChild(button);
+    document.querySelector(".keyboard").appendChild(button);
     // e.prevent default fixed an error where page was refreshing
     button.addEventListener("click", e => initGame(e.target, String.fromCharCode(index), e.preventDefault()));
 }
